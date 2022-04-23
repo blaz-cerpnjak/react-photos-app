652f74ef55a560613e9144f3aa8850cfa0567d2d
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-04-23 13:44:54 +0200

    Change Add Photo Design

--- frontend/src/components/AddPhoto.js
+++ frontend/src/components/AddPhoto.js
@@ -1,9 +1,10 @@
-import { Container, IconButton } from '@mui/material';
+import { Alert, AlertTitle, Button, Container, IconButton } from '@mui/material';
 import { useContext, useState } from 'react'
 import { Navigate } from 'react-router';
 import { UserContext } from '../userContext';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import { styled } from '@mui/material/styles';
+import AddIcon from '@mui/icons-material/Add';
 
 const Input = styled('input')({
     display: 'none',
@@ -11,20 +12,23 @@ const Input = styled('input')({
 
 function AddPhoto(props) {
     const userContext = useContext(UserContext); 
-    const[name, setTitle] = useState('');
-    const[file, setFile] = useState('');
-    const[uploaded, setUploaded] = useState(false);
+    const [title, setTitle] = useState('');
+    const [file, setFile] = useState('');
+    const [error, setError] = useState("");
+    const [isError, showError] = useState(false);
+    const [uploaded, setUploaded] = useState(false);
 
     async function onSubmit(e){
         e.preventDefault();
 
-        if(!name){
-            alert("Vnesite ime!");
+        if (!title){
+            setError("You must specify image title.");
+            showError(true);
             return;
         }
 
         const formData = new FormData();
-        formData.append('name', name);
+        formData.append('name', title);
         formData.append('image', file);
         const res = await fetch('http://localhost:3001/photos', {
             method: 'POST',
@@ -43,15 +47,24 @@ function AddPhoto(props) {
             <form className="form-group" onSubmit={onSubmit}>
                 {!userContext.user ? <Navigate replace to="/login" /> : ""}
                 {uploaded ? <Navigate replace to="/" /> : ""}
-                <input type="text" className="form-control" name="ime" placeholder="Title" value={name} onChange={(e)=>{setTitle(e.target.value)}}/>
-                <label>Izberi sliko</label>
-                <input type="file" id="file" onChange={(e)=>{setFile(e.target.files[0])}}/>
-                <Input accept="image/*" id="icon-button-file" type="file" />
-                <IconButton color="primary" aria-label="upload picture" component="span">
-                    <PhotoCamera />
-                </IconButton>
-                <input className="btn btn-primary" type="submit" name="submit" value="Naloži" />
+                <input type="text" className="form-control" name="title" placeholder="Title" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+                <br></br>
+                <label htmlFor="contained-button-file">
+                    <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={(e) => {setFile(e.target.files[0])}} />
+                    <Button startIcon={<PhotoCamera/>} variant="contained" component="span">Select Photo</Button>
+                </label>
+                <br></br>
+                <br></br>
+                <Button startIcon={<AddIcon/>} variant="contained" onClick={onSubmit}>Upload</Button>
             </form>
+            <br></br>
+            { isError ?
+                <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    {error} <strong>Please try again!</strong>
+                </Alert> 
+                : ""
+            }
         </Container>
         </>
     )
